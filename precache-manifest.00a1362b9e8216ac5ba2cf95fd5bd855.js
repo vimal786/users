9ccacb92c459c151a28607d98bda18b86e89d339
vimal786 <vimalimr@gmail.com>
2020-09-29 10:42:27 +0530

    Updates

---
+++ precache-manifest.00a1362b9e8216ac5ba2cf95fd5bd855.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e8444406e1978d8f8cf6bdc871d681",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "1d0159429769d6a63138",
+    "url": "/users/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ddcf3e3939b91e6e10ab",
+    "url": "/users/static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "1d0159429769d6a63138",
+    "url": "/users/static/js/2.f342c6bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/users/static/js/2.f342c6bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddcf3e3939b91e6e10ab",
+    "url": "/users/static/js/main.99e5a6e9.chunk.js"
+  },
+  {
+    "revision": "76492a2e6935e7af03e9",
+    "url": "/users/static/js/runtime-main.b4d70fad.js"
+  }
+]);
